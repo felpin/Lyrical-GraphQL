b7baa3046acedc3ff4506c005b1e59ded4b6ac9f
Felipe de Alencar Pinheiro <felipe.pinheiro.90@hotmail.com>
2018-03-21 22:17:52 -0300

    Adds SongList as base component of app

---
+++ client/components/SongList.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+class SongList extends PureComponent {
+  render() {
+    return (
+      <div>
+        This will be my song list
+      </div>
+    );
+  }
+}
+
+export default SongList;
