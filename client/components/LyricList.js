677fef7158d3d2241b75da6dbe3aa163c974ad76
Felipe de Alencar Pinheiro <felipe.pinheiro.90@hotmail.com>
2018-03-25 20:24:15 -0300

    Starts to develop the like lyric button

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,10 +1,20 @@
 import React, { PureComponent } from 'react';
 
 class LyricList extends PureComponent {
+  onLike() {
+    // TODO: Implement function
+  }
+
   renderLyrics() {
     return this.props.lyrics.map(({ id, content }) => (
       <li key={id} className="collection-item">
         {content}
+        <i
+          className="material-icons right"
+          onClick={() => this.onLike(id)}
+        >
+          thumb_up
+        </i>
       </li>
     ));
   }
