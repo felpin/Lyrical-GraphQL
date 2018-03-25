f58542a5e1b5a41213a48d0d1cb8a25398723882
Felipe de Alencar Pinheiro <felipe.pinheiro.90@hotmail.com>
2018-03-25 11:43:19 -0300

    Writes graphql mutation to LyricCreate

--- client/components/LyricCreate.js
+++ client/components/LyricCreate.js
@@ -1,14 +1,47 @@
+import gql from 'graphql-tag';
 import React, { PureComponent } from 'react';
 
 class LyricCreate extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = { content: '' };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(event) {
+    this.setState({ content: event.target.value });
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+
+
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.onSubmit}>
         <label>Add a lyric</label>
-        <input />
+        <input
+          value={this.state.content}
+          onChange={this.onChange}
+        />
       </form>
     );
   }
 }
+
+const mutation = gql`
+mutation AddLyricToSong($content: String, $songId: ID) {
+  addLyricToSong(content: $content, songId: $songId) {
+    id
+    lyrics {
+      content
+    }
+  }
+}
+`;
 
 export default LyricCreate;
